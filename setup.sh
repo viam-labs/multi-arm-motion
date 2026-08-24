c09cc74fa1fa3bf4df467863ea23eb28c39c3c6c
JosephBorodach <joseph.borodach@viam.com>
2026-08-24 10:56:30 -0400

    Add lint + build/test CI

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+OS="$(uname -s)"
+if [[ "$OS" == "Linux" ]]; then
+    if ! dpkg -l libnlopt-dev >/dev/null 2>&1; then
+        sudo apt-get update || true
+        sudo apt-get install -y --no-install-recommends \
+            -o DPkg::Lock::Timeout=60 \
+            ca-certificates \
+            libnlopt-dev
+    fi
+elif [[ "$OS" == "Darwin" ]]; then
+    if [ ! -f /opt/homebrew/include/nlopt.h ] && [ ! -f /usr/local/include/nlopt.h ]; then
+        brew tap viamrobotics/brews
+        brew install nlopt-static
+    fi
+fi
